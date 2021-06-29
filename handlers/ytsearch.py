c7dd9fe921e08a9f0a3c5eb20d3263261264f60c
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-06-29 15:14:45 +0700

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -20,19 +20,20 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("❁ **Music** tidak ditemukan\n❁ **Ketik** /search (**judul lagu**)")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Mencari....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"Judul : {results[i]['title']}\n"
+            text += f"Durasi : {results[i]['duration']}\n"
+            text += f"Views : {results[i]['views']}\n"
+            text += f"Channel : {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
+            text += f"─────────────────────────────\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
