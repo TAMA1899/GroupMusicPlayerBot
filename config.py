1f5608d4687554ccb381d2e248f23760faef0d7d
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-06-29 22:17:25 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 from os import getenv
-
+import os
 from dotenv import load_dotenv
 
-load_dotenv()
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
+que = {}
+admin = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
