e4e1edc33c226039ed0949023b1bcae5efe678a7
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-06-30 07:10:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,13 @@
 from os import getenv
-import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
-
 que = {}
-admin = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
-
+admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
