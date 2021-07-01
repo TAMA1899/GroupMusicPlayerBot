cf956c791e87938f8eb965c041d0225c5d6a7a93
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-07-01 13:30:26 +0700

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -4,7 +4,6 @@ from pyrogram import Client
 from pyrogram.types import Message
 from callsmusic import callsmusic
 
-from config import BOT_NAME as BN
 from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
 
