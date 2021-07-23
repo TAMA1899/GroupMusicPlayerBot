2eeebeba98817a1e4b877f0e6de384878fe8b961
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-07-24 00:48:50 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -214,7 +214,7 @@ async def play(_, message: Message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "GeezProject"
+        user.first_name = "helper"
     usar = user
     wew = usar.id
     try:
