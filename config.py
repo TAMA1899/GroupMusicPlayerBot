62398ee8cbd4859072f58877a849f1a456b3f7be
TAMA1899 <60563853+TAMA1899@users.noreply.github.com>
2021-07-24 01:11:44 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "AQCKsKNW6LY70PSUjtYgWS-z0luPGL2TRkv0s3XYSOeIqVWKLOlM5FzCfqch1GfVORIlH-nyFlJ61suiWvNT6GaUdbJ3a95auZv5jomPUj3g0_lJZPk6dRGiBvngRvIB0IyPIJNj2dTlf2g3q2AIMGALQH-no2LqMKxVlGjeIRb-jtXPl7PFpWobcr7-HVbzKvgpfcohUHbQDb1O9-BetvBT_KCYMMGZn43Y-0Mk-pavEMSk7VsdHYCVU0uRc4mjMSQ3TL0wjO7oymiUudW0jr1ERO7QtnLaH8MTzY0gHOatBNUk72ASl7QmclKnFgBxndyLXwborswjCavAIXUoRs9bSW3WRQA")
-BOT_TOKEN = getenv("BOT_TOKEN", "1858649461:AAEJu3U-Vnc_vvogVaAiskcN1PUGKQROTDg")
+SESSION_NAME = getenv("SESSION_NAME", "BQCn7FaEWbrboGeMRVV3UnvCG33Qcxrg-XZmow18K1uWw3RM9579y86bM7ynwAGw8eRE5Ae5W78E-ZmH1sUG2U_unacfNzmoNg9-_pvalKylGwVjmonvuI70sjipS4NyqKZT03T3wVttROBn4vF_WYYw8Pr1Dzke_N8XA65aNqikxCaddSYfOkytOZhp-J3aZWcT02iAZnJeuHAOKYlnTv8FeOmH6jLKkr_TkLrNs3Kal-6neCNgfZa8-MzwcW8R_vVAZ2P8KC0Ed48jKBJUP_oScxBDxTTA3Q6_RAHTuWxENMDPP6E5AkQPO8d8kOFVXvr4964d5LBfd10aCJw57GIYO0OarQA")
+BOT_TOKEN = getenv("BOT_TOKEN", "1864672623:AAE6cXQhtiWwu6OW2Cls3pffiNTZcCwIIls")
 
-API_ID = int(getenv("API_ID", "3371945"))
-API_HASH = getenv("API_HASH", "880695522786b34a4e943902db6e4f64")
+API_ID = int(getenv("API_ID", "3448718"))
+API_HASH = getenv("API_HASH", "5e6d19055850eb5b35720a87e798d0b0")
 
-BOT_NAME = getenv("BOT_NAME", "𝐂𝐚𝐧𝐝𝐮𝐌𝐮𝐬𝐢𝐜")
+BOT_NAME = getenv("BOT_NAME", "𝐑𝐨𝐛𝐨𝐭𝐌𝐮𝐬𝐢𝐜")
 
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "15"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1652454077").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "994286253").split()))
